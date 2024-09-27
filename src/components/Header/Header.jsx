56538f0bb221e65b5916d568b536421848159290
DamianLavin <damianmlavin@gmail.com>
2024-09-27 05:20:12 -0700

    Links coded.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import "./Header.css";
 
 import logo from "../../images/Logo.svg";
@@ -12,8 +14,9 @@ function Header({ handleAddClick, handleProfileClick, weatherData }) {
 
   return (
     <header className="header">
-      <img src={logo} alt="Logo" className="header__logo" />
-
+      <Link to={"/"}>
+        <img src={logo} alt="Logo" className="header__logo" />
+      </Link>
       <p className="header__date-and-location">
         {currentDate}, {weatherData.city}
       </p>
@@ -27,18 +30,19 @@ function Header({ handleAddClick, handleProfileClick, weatherData }) {
       >
         + Add clothes
       </button>
-
-      <div className="header__user-container">
-        {/* <p className="header__username">Terrence Tegegne</p> */}
-        <button
-          onClick={handleProfileClick}
-          type="button"
-          className="header__username"
-        >
-          Terrence Tegegne
-        </button>
-        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
-      </div>
+      <Link to={"/profile"} className="header__link">
+        <div className="header__user-container">
+          {/* <p className="header__username">Terrence Tegegne</p> */}
+          <button
+            onClick={handleProfileClick}
+            type="button"
+            className="header__username"
+          >
+            Terrence Tegegne
+          </button>
+          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+        </div>
+      </Link>
     </header>
   );
 }
