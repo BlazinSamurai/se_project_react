90855216f4128af9a3b5afdcf770f70798c055ae
DamianLavin <damianmlavin@gmail.com>
2024-11-04 13:23:32 -0800

    P4.S11.s8.v5

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -9,8 +9,6 @@ function ModalWithForm({
   onSubmit,
 }) {
   return (
-    //isOpen={activeModal === "add-garment"}
-    // <div className={`modal ${isOpen === "add-garment" && "modal_opened"}`}>
     <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
